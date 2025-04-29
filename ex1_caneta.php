4a175fc3a4cbc65a78ca0f17e183be543224e2aa
Bruno Siqueira da Silva <bruno2siqueira@users.noreply.github.com>
2025-04-29 09:26:38 -0300

    Add files via upload

---
+++ ex1_caneta.php
@@ -0,0 +1,48 @@
+<?php
+
+// Definindo a classe Caneta
+
+class Caneta{
+    // Atributos
+    public $cor;
+    public $marca;
+    public $ponta;
+    public $tamanho;
+    public $carga;
+
+    // Métodos
+    public function escrever(){
+        echo "Escrevendo...<br>";
+    }
+    public function rabiscar(){
+        echo "Rabiscando...<br>";
+    }
+    public function sublinar(){
+        echo "Sublinhando...<br>";
+    }
+    public function pintar(){
+        echo "Pintando...<br>";
+    }
+
+}
+
+// Instanciando a classe Caneta
+$caneta1 = new Caneta();
+//como os atributos da classe Caneta são públicos, podemos acessá-los diretamente
+$caneta1->cor = "azul";
+$caneta1->marca = "BIC";
+$caneta1->ponta = 0.5;
+$caneta1->tamanho = "grande";
+$caneta1->carga = 100;
+// Exibindo os atributos da caneta
+echo "Cor: " . $caneta1->cor . "<br>";
+echo "Marca: " . $caneta1->marca . "<br>";
+echo "Ponta: " . $caneta1->ponta . "<br>";
+echo "Tamanho: " . $caneta1->tamanho . "<br>";
+echo "Carga: " . $caneta1->carga . "<br>";
+// Chamando os métodos da caneta
+$caneta1->escrever();
+$caneta1->rabiscar();
+$caneta1->sublinar();
+$caneta1->pintar();
+?>
